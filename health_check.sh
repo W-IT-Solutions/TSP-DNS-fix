d64c6bd00ccf419e8e712ef762684c2f9291f614
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2022-01-30 21:55:42 +0100

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -44,19 +44,19 @@ header() {
 fatal() {
 	/bin/echo -e "${IRed} $* ${Color_Off}" >&2
   	/bin/echo -e "${IRed} $* ${Color_Off}" >> /var/log/health_check_script_errors_warnings.log    
-	if [ -f /tmp/.script_lock_"$INTERFACE" ]; then
-		rm -rf /tmp/.script_lock_"$INTERFACE" && success "$(date) - $INTERFACE - Removed /tmp/.script_lock_$INTERFACE prematurely" || fatal "$(date) - $INTERFACE - Failed to remove /tmp/.script_lock_$INTERFACE"
+	if [ -f /tmp/.script_lock_"$1" ]; then
+		rm -rf /tmp/.script_lock_"$1" && success "$(date) - $1 - Removed /tmp/.script_lock_$1 prematurely" || fatal "$(date) - $1 - Failed to remove /tmp/.script_lock_$1"
 	fi
 	exit 1
 }
 
 ################################ Check / Set lock
-#if [ -f /tmp/.script_lock_"$1" ]; then
-#    error "$(date) - $1 - Script is already running"
-#	exit 1
-#else
-#    touch /tmp/.script_lock_"$1" && start "$(date) - $1 - Created /tmp/.script_lock_$1" || fatal "$(date) - $1 - Failed to create /tmp/.script_lock_$1"
-#fi
+if [ -f /tmp/.script_lock_"$1" ]; then
+    	error "$(date) - $1 - Script is already running"
+	exit 1
+else
+   	 touch /tmp/.script_lock_"$1" && start "$(date) - $1 - Created /tmp/.script_lock_$1" || fatal "$(date) - $1 - Failed to create /tmp/.script_lock_$1"
+fi
 
 ################################ Interface parameter check
 start "$(date) - $1 - Health check initiated"
@@ -72,9 +72,7 @@ DEBUG="0" # 1 = on / 0 = off
 IP=$(ip address show dev "$INTERFACE" | grep inet | head -1 | awk '{printf "%s\n",$2}' | sed 's|/24||g')
 #GW='192.168.8.1'
 #NETWORK='192.168.8' # Leave OUT the last dot and octet 
-#LOG='/var/log/if_down_checker.log'
-#WAITSEC="60" # Loop time, every 10 seconds
-#CURLTIMEOUT="20" 
+WAITSEC="10" # Loop time, every 10 seconds
 METRIC=$(route -n | grep 'UG' | grep "$INTERFACE" | awk '{printf "%s\n",$5}')
 octet=$(ip address show dev "$INTERFACE" | grep inet | awk -F "[/.]" '{print $4;}')
 tablenum=$(( $octet + 100 ))
@@ -122,14 +120,6 @@ debug_mode() {
 debug_mode
 root_check
 
-################################ Check / Set lock
-if [ -f /tmp/.script_lock_"$1" ]; then
-    	error "$(date) - $1 - Script is already running"
-	exit 1
-else
-    	touch /tmp/.script_lock_"$1" && start "$(date) - $1 - Created /tmp/.script_lock_$1" || fatal "$(date) - $1 - Failed to create /tmp/.script_lock_$1"
-fi
-
 ################################ Delete routes
 #ip route delete default via "$GW" dev "$INTERFACE" src "$IP" && success "$(date) - $INTERFACE - Deleted routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to delete routes for $INTERFACE"
 #route -vF del -net "$NETWORK".0/24 gw 0.0.0.0 
@@ -151,33 +141,29 @@ dhcpcd -m 10"${tablenum}" "$INTERFACE" && success "$(date) - $INTERFACE - Metric
 #    my_clear_cmd # "$dest_addr" "$latency_avg" "$loss_avg"
 #fi
 
-
 ################################ Loop checking for connectivity and adding routes on downed interface
 while :
 do
 	header "$(date) - $INTERFACE - Loop, checking for connectivity"
-	# Bring the interface up again in order to test for a connection
-	#ip link set "$INTERFACE" up && success "$(date) - $INTERFACE - is brought up" || fatal "$(date) - $INTERFACE - Failed to bring up $INTERFACE"	
 	
 	# Check the loss % on the interface
 	CONNECTION=$(cat "/tmp/health_$INTERFACE" | awk '{print $3}')
 
-	if [ "$CONNECTION" -lt "0"; then
-			#ip route add default via "$GW" dev "$INTERFACE" src "$NETWORK"."$octet" table "$tablenum" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
-			#if ! ip rule show | grep -q "$NETWORK.${octet}"; then
-				#ip rule add from "$NETWORK"."${octet}" table "${tablenum}" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
-		    	#fi
+	if [ "$CONNECTION" == "0"; then
+		#ip route add default via "$GW" dev "$INTERFACE" src "$NETWORK"."$octet" table "$tablenum" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
+		#if ! ip rule show | grep -q "$NETWORK.${octet}"; then
+			#ip rule add from "$NETWORK"."${octet}" table "${tablenum}" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
+		#fi
 
-			# Loss is 0% set original metric value back
-			dhcpcd -m "$METRIC" "$INTERFACE" && success "$(date) - $INTERFACE - Metric set to old value of: $METRIC" || fatal "$(date) - $INTERFACE - Failed to set metric to old value of: $METRIC"
-			dhcpcd -n "$INTERFACE"
-			
-			# Abandon the loop.
-			success "$(date) - $INTERFACE - Break the loop"
-			break 
+		# Loss is 0% set original metric value back
+		dhcpcd -m "$METRIC" "$INTERFACE" && success "$(date) - $INTERFACE - Metric set to old value of: $METRIC" || fatal "$(date) - $INTERFACE - Failed to set metric to old value of: $METRIC"
+		dhcpcd -n "$INTERFACE"
+
+		# Abandon the loop.
+		success "$(date) - $INTERFACE - Break the loop"
+		break 
 	else
-
-		#ifconfig "$INTERFACE" down && error "$(date) - $INTERFACE - $INTERFACE is brought down, still no connection" || fatal "$(date) - $INTERFACE - Failed to bring down $INTERFACE"
+		error "$(date) - $INTERFACE - Loss % is still higher then threshold"
   	fi
 
 	header "$(date) - $INTERFACE - Eat, sleep, bash, repeat"
@@ -191,6 +177,6 @@ fi
 
 # End of script
 success "$(date) - Script finished - $COUNTER Warning(s) and / or error(s)"
-cat /var/log/health_check_script_errors_warnings.log
+#cat /var/log/health_check_script_errors_warnings.log
 
 exit 0
