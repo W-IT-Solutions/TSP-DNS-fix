acd0970a3d78e3e1412893ca8fe9d052a68d6d16
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2022-01-31 10:00:30 +0100

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -6,7 +6,7 @@
 # For reference
 
 ################################ Logger
-INTERACTIVE="0" # 1 = on / 0 = off - Log all script output to file (0) or just output everything in stout (1)
+INTERACTIVE="1" # 1 = on / 0 = off - Log all script output to file (0) or just output everything in stout (1)
 if ! [ "$INTERACTIVE" == 1 ]; then 
     LOGFILE="/var/log/health_check.log" # Log file
     exec 3>&1 4>&2
@@ -16,7 +16,7 @@ fi
 
 ################################ CMD line output
 print_text_in_color() {
-	/usr/bin/printf "%b%s%b\n" "$INTERFACE" "$2" "$Color_Off"
+	/usr/bin/printf "%b%s%b\n" "$1" "$2" "$Color_Off"
 }
 Color_Off='\e[0m'   # Text Reset
 IRed='\e[0;91m' 		# Red
@@ -55,11 +55,11 @@ if [ -f /tmp/.script_lock_"$1" ]; then
     	error "$(date) - $1 - Script is already running"
 	exit 1
 else
-   	 touch /tmp/.script_lock_"$1" && start "$(date) - $1 - Created /tmp/.script_lock_$1" || fatal "$(date) - $1 - Failed to create /tmp/.script_lock_$1"
+   	 touch /tmp/.script_lock_"$1" && success "$(date) - $1 - Created /tmp/.script_lock_$1" || fatal "$(date) - $1 - Failed to create /tmp/.script_lock_$1"
 fi
 
 ################################ Interface parameter check
-start "$(date) - $1 - Health check initiated"
+header "$(date) - $1 - Health check initiated"
 if [ -n "$1" ]; then
 	INTERFACE="$1"
 	success "$(date) - $INTERFACE - Interface parameter detected"
@@ -68,7 +68,7 @@ else
 fi
 
 ################################ Vars and functions
-DEBUG="0" # 1 = on / 0 = off
+DEBUG="1" # 1 = on / 0 = off
 IP=$(ip address show dev "$INTERFACE" | grep inet | head -1 | awk '{printf "%s\n",$2}' | sed 's|/24||g')
 #GW='192.168.8.1'
 #NETWORK='192.168.8' # Leave OUT the last dot and octet 
@@ -128,9 +128,6 @@ root_check
 ################################ Bring down the interface
 #ip link set "$INTERFACE" down && success "$(date) - $INTERFACE - $INTERFACE is brought down" || fatal "$(date) - $INTERFACE - Failed to bring down $INTERFACE"
 
-################################ Set metric higher since we have loss or high latency
-dhcpcd -m 10"${tablenum}" "$INTERFACE" && success "$(date) - $INTERFACE - Metric set to: 10${tablenum}" || fatal "$(date) - $INTERFACE - Failed to set metric to: 10${tablenum}"
-
 ################################ Automate based on alert_flag
 # the alert_cmd is invoked as "alert_cmd dest_addr alarm_flag latency_avg loss_avg"
 # alarm_flag is set to 1 if either latency or loss is in alarm state
@@ -147,25 +144,36 @@ do
 	header "$(date) - $INTERFACE - Loop, checking for connectivity"
 	
 	# Check the loss % on the interface
-	CONNECTION=$(cat "/tmp/health_$INTERFACE" | awk '{print $3}')
-
-	if [ "$CONNECTION" == "0"; then
+	CONNECTION=$(cat "/tmp/health_$INTERFACE" | awk '{print $5}')
+	
+	if ! [ -f /tmp/health_$INTERFACE ]; then
+		error "$(date) - $INTERFACE - Failed to read /tmp/health_$INTERFACE"
+	else
+		success "$(date) - $INTERFACE - Read /tmp/health_$INTERFACE: loss % is at: $CONNECTION"
+	fi
+	
+	if [ "$CONNECTION" == "0" ]; then
 		#ip route add default via "$GW" dev "$INTERFACE" src "$NETWORK"."$octet" table "$tablenum" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
 		#if ! ip rule show | grep -q "$NETWORK.${octet}"; then
 			#ip rule add from "$NETWORK"."${octet}" table "${tablenum}" && success "$(date) - $INTERFACE - Added routes for $INTERFACE" || fatal "$(date) - $INTERFACE - Failed to add routes for $INTERFACE"
 		#fi
 
 		# Loss is 0% set original metric value back
-		dhcpcd -m "$METRIC" "$INTERFACE" && success "$(date) - $INTERFACE - Metric set to old value of: $METRIC" || fatal "$(date) - $INTERFACE - Failed to set metric to old value of: $METRIC"
-		dhcpcd -n "$INTERFACE"
+		ifmetric "$INTERFACE" "$METRIC" && success "$(date) - $INTERFACE - Metric set to old value of: $METRIC" || fatal "$(date) - $INTERFACE - Failed to set metric to old value of: $METRIC"
+		#/sbin/dhcpcd -n "$INTERFACE"
 
 		# Abandon the loop.
 		success "$(date) - $INTERFACE - Break the loop"
 		break 
-	else
-		error "$(date) - $INTERFACE - Loss % is still higher then threshold"
-  	fi
+	elif [ "$CONNECTION" -gt "5" ]; then
+		error "$(date) - $INTERFACE - Loss % is still higher then threshold - CONNECTION -gt 5"
+		
+		# Set metric higher since we have loss or high latency
+		#/sbin/dhcpcd -m 1"${tablenum}" "$INTERFACE" && success "$(date) - $INTERFACE - Metric set to: 10${tablenum}" || fatal "$(date) - $INTERFACE - Failed to set metric to: 10${tablenum}"
+		ifmetric "$INTERFACE" 1"${tablenum}" && success "$(date) - $INTERFACE - Metric set to: 10${tablenum}" || fatal "$(date) - $INTERFACE - Failed to set metric to: 10${tablenum}"
 
+	fi
+	
 	header "$(date) - $INTERFACE - Eat, sleep, bash, repeat"
 	sleep "$WAITSEC"
 done
